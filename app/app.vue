889be63b09a77f94c0e58fe951a66e7e87e38e39
pixelliquid <peter@pixelliquid.com>
2025-01-20 17:04:15 +0100

    exercise gallery

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,40 @@
-<script></script>
+<script>
+import { defineNuxtComponent } from '#app' // helpful when using options API
+
+export default defineNuxtComponent({
+  data: () => ({
+    photosList: []
+  }),
+  methods: {
+    fetchPhotosList() {
+      fetch('https://jsonplaceholder.typicode.com/photos/')
+        .then(response => response.json())
+        .then(json => {
+          this.photosList = json
+        })
+    }
+  }
+})
+</script>
 
 <template>
+  <img src="/working.jpg" alt="working with kitten Chef" />
   <h1>Photo Gallery</h1>
+  <button @click="fetchPhotosList">Fetch Photos</button>
+  <ul>
+    <li v-for="photo in photosList.slice(0, 20)" :key="`photo-id-${photo.id}`">
+      <img :src="photo.thumbnailUrl" :alt="`${photo.title}`" />
+    </li>
+  </ul>
+  <pre>{{ photosList }}</pre>
 </template>
 
-<style></style>
+<style>
+  img {
+    width: 100%;
+  }
+  ul {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+  }
+</style>
