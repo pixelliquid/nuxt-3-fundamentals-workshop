30c56a60c9d22edca8ca86ba62a1a98e62fa22f3
pixelliquid <peter@pixelliquid.com>
2025-01-20 17:37:02 +0100

    include some computed properties

--- app/app.vue
+++ app/app.vue
@@ -17,6 +17,21 @@ export default defineNuxtComponent({
       //   this.photoGallery = response.json()
       // })
     }
+  },
+  // computed properties to show amount of data or filter the data in the json
+  computed: {
+    numberOfPhotos() {
+      return this.photoGallery.length
+    },
+    evenAlbums() {
+      return this.photoGallery.filter(photo => photo.albumId % 2 === 0)
+    },
+    oddAlbums() {
+      return this.photoGallery.filter(photo => photo.albumId % 2 !== 0)
+    },
+    evenAlbumPercentage() {
+      return (this.evenAlbums.length / this.numberOfPhotos) * 100
+    },
   }
 })
 </script>
@@ -24,6 +39,8 @@ export default defineNuxtComponent({
 <template>
   <h1>Photo Gallery</h1>
   <button @click="fetchPhotoGallery">Fetch Photos</button>
+  <pre>{{ numberOfPhotos }} photos, {{ oddAlbums.length }} odd albums & {{ evenAlbums.length }} even albums
+  ({{ evenAlbums.length }} even albums is {{ evenAlbumPercentage }}% of the whole dataset)</pre>
   <ul>
     <li v-for="photo in photoGallery.slice(0, 20)" :key="`photo-id-${photo.id}`">
       <img :src="photo.thumbnailUrl" :alt="`${photo.title}`" />
