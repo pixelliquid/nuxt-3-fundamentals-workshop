4cb222951330462545558f8a9d1aa4181c2f87af
pixelliquid <peter@pixelliquid.com>
2025-01-23 16:10:39 +0100

    show active page in nav

--- app/app.vue
+++ app/app.vue
@@ -8,4 +8,10 @@
 <style lang="scss">
 @import './node_modules/bulma/bulma.sass';
 @import './assets/styles/main.scss';
+
+.router-link-active,
+a.navbar-item:hover,
+a.navbar-item:focus {
+  color: deeppink;
+}
 </style>
