5f1a4f110cd6f5dd25a5b649ea36b0335d686e04
pixelliquid <peter@pixelliquid.com>
2025-01-23 16:57:18 +0100

    revert back to nested folder routing

--- app/pages/display/index.vue
+++ app/pages/display/index.vue
@@ -4,10 +4,6 @@
   <div class="container">
     <div class="section">
       <h1 class="title">Display</h1>
-      <h2 class="title">Todo Viewer</h2>
-      <TodoViewer />
-      <h2 class="title">Photos</h2>
-      <PhotoGallery />
     </div>
   </div>
 </template>
