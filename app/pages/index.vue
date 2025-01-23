afe2c42b8b83a8e869414fb4ae394186f461603a
pixelliquid <peter@pixelliquid.com>
2025-01-23 16:37:37 +0100

    remove in-page navigation

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,8 +4,6 @@
   <div class="container">
     <div class="section">
       <h1 class="title">Home</h1>
-      <NuxtLink to="/display/photos" class="navbar-item">Photos</NuxtLink>
-      <NuxtLink to="/display/todos" class="navbar-item">Todos</NuxtLink>
     </div>
   </div>
 </template>
