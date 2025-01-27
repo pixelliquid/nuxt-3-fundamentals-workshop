3858a642535de1f3473478724eb5a0475905636a
pixelliquid <peter@pixelliquid.com>
2025-01-27 15:15:56 +0100

    follow along with Ben on useAsyncData()

--- app/components/BaseDisplay.vue
+++ app/components/BaseDisplay.vue
@@ -22,14 +22,20 @@ const route = useRoute()
 const itemType = route.path.split('/')[2] // use the last bit in the path which is the name of the file within the display folder
 
 // onMounted(() => { // lifeCycle hook which is triggered when the component is mounted, just fetch() ... in setup block works as well
-function fetchData() { // function which is triggered by a click on the appropriate button
-  fetch(`https://jsonplaceholder.typicode.com/${itemType}`)
-    .then(response => response.json())
-    .then(json => {
-      emit('update:itemList', json)
-    })
-}
+// function fetchData() { // function which is triggered by a click on the appropriate button
+// useAsyncData is a composable to use fetch on node server which triggers on setup
+useAsyncData(`${itemType}Query`, () => {
+  $fetch(`https://jsonplaceholder.typicode.com/${itemType}`)
+    // .then(response => console.log(response)) // log response to check its structure -> nuxt jsonifies it
+    .then(response => emit('update:itemList', response)) // we only need to emit it without any further processing
+  // otherwise we had to process this code:
+  // .then(response => response.json())
+  // .then(json => {
+  //   emit('update:itemList', json)
+  // })
+})
 
+// with useAsyncData() the clearData does not work anymore, because the data is fetched from the server and is not stored in the component
 function clearData() {
   emit('update:itemList', [])
 }
