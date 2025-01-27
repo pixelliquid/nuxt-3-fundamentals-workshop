435b2d86f2c99be74daefbdfbc63e05a328c0453
pixelliquid <peter@pixelliquid.com>
2025-01-27 14:52:24 +0100

    font resource exercise

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,22 @@
 <script setup>
+// useHead() is a composable for page metadata
 useHead({
   title: 'Hello world'
 })
 </script>
 
 <template>
+
+  <!-- Nuxt config for page metadata -->
+  <Html lang="">
+  <Title>Merriweather Font</Title>
+  <Link rel="preconnect" href="https://fonts.googleapis.com" />
+  <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <Link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" />
+
+  </Html>
+
   <div class="container">
     <div class="section">
       <NuxtLayout>
@@ -14,4 +26,8 @@ useHead({
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+body {
+  font-family: 'Merriweather', serif;
+}
+</style>
